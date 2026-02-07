59602e6a15d762796c67e1371938765d3e8d11fc
i love docker <falhou77@gmail.com>
2026-02-07 13:39:55 -0300

    Fixed src/index.ts
    Sorry

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-import { Client } from "./core/client.js";
-
 export { Client } from "./core/client.js";
-
-const client = new Client({
-    suggestions: {
-        enabled: true,
-        amount: 5,
-        verification: true
-    }
-});
-
-const result = await client.roblox("fa1hou");
-console.log(result);
